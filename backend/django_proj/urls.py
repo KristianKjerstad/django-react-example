fc9c96e1f1b50a8bf5299e58b67d4df66e0c77b9
Kristian <kristian.kjerstad@gmail.com>
2023-10-10 08:22:08 +0000

    feat: add students api endpoints

--- backend/django_proj/urls.py
+++ backend/django_proj/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/students/", include("students.urls"))
+    # re_path(r"^api/students/([0-9])$", views.students_detail),
 ]
--- backend/students/views.py
+++ backend/students/views.py
@@ -1,1 +1,42 @@
-# Create your views here.
+import json
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
+from rest_framework import status
+
+from students.serializers import StudentSerializer
+
+from .models import Student
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class StudentsView(TemplateView):
+    template_name = "students"
+
+    def get(self, request):
+        """Get all students"""
+        data = Student.objects.all()
+        serializer = StudentSerializer(data, context={"request": request}, many=True)
+        return HttpResponse(json.dumps(serializer.data), status=status.HTTP_200_OK, content_type="application/json")
+
+    def post(self, request):
+        """Add a single student"""
+        print(json.loads(request.body))
+        serializer = StudentSerializer(data=json.loads(request.body))
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return HttpResponse(status=status.HTTP_201_CREATED)
+        return HttpResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StudentView(TemplateView):
+    template_name = "student"
+
+    def get(self, request, student_id):
+        """Get a single student"""
+        data = get_object_or_404(Student, pk=student_id)
+        serializer = StudentSerializer(data, context={"request": request}, many=False)
+        return HttpResponse(json.dumps(serializer.data), status=status.HTTP_200_OK, content_type="application/json")
